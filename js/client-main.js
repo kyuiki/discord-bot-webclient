b4d663d347ef0d032633218cf3bceaac88465ce4
qky <qkie0915@gmail.com>
2020-02-22 09:36:17 +0700

    first commit

---
+++ js/client-main.js
@@ -0,0 +1,77 @@
+
+		var token = prompt("please put the Bot Token, Please put the real token or it willnt work");
+		if(!token) {alert("This client wouldnt start with out your bot token! refresh this page to try again")};
+		const client = new Discord.Client();
+
+		client.on('ready', () => {
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+			var format = `(${date}) ${color("[CLIENT READY!] ", "#00ff00")} The client is now ready. Chat in your server discord (with your bot in it) and your bot will fetch it xD`;
+			texting(format);
+		});
+		client.on('message', msg =>{
+			if(msg.author.bot && msg.author.id !== client.user.id) return ;
+			const guildTag = msg.channel.type === 'text' ? color(`${msg.guild.name}`, "lightgreen") : color("Discord", "red");
+
+			const channelTag = msg.channel.type === 'text' ? 
+			color(`${msg.channel.name}`, "orange"): color("Direct Message", "purple");var attach = "";
+
+			if(msg.attachments.size){
+				for(var i = 0; i<msg.attachments.size; i++){
+				attach += `<img class="attachment" src="${msg.attachments.array()[i].url}">`
+					}
+			}
+			/*if(msg.content.match(/@\d+>/gi)){
+				for(var i = 0; i+1<msg.content.match(/@\d+>/gi);i++){
+					x =
+				}
+			}*/
+
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+
+			texting(`<img class="profile" src="${msg.author.avatarURL}"/> (${color(date, "white")}) <span class="clicky" title="Delete The Message" onclick="del('${msg.channel.id}', '${msg.id}');this.parentElement.style.display = 'none';">${color("[MESSAGE]", "lightblue")}</span> &nbsp;<span class="clicky" title="Send a message to this channel" onclick="gm('${msg.channel.id}')">[${guildTag}/${channelTag}</span>] &nbsp;|| &nbsp;<span title="Send a message to this User" class="clicky" onclick="dm('${msg.author.id}')">${color(msg.channel.type === "text" ? color(msg.member.displayName, msg.member.displayHexColor) : msg.author.tag, "#ffffff")}</span> &nbsp;>>&nbsp; ${color(msg.content.split("\n").join("<br/>"), "#aaaaaa")} ${attach}`, msg.content, msg)
+		});
+		client.on('messageDelete', msg =>{
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+			var attach = "";
+
+			if(msg.attachments.size){
+				for(var i = 0; i<msg.attachments.size; i++){
+				attach += `<img class="attachment" src="${msg.attachments.array()[i].url}">`
+					}
+			}
+
+			var format = `(${date}) ${color("[MESSAGE DELETED!]", "red")}  <b>"${msg.member.displayName}"</b>'s message deleted in [${color(msg.guild.name, "lightgreen")+"/"+color(msg.channel.name, "orange")}] with content&nbsp; >> ${color(msg.content.split("\n").join("<br/>"), "#aaaaaa")} ${attach}`;
+			texting(format);
+		});
+		client.on("guildMemberUpdate", (o, n) =>{
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+			var format = `(${date}) ${color("[MEMBER UPDATED!] ", "red")} <b>"${color(o.displayName, o.displayHexColor)}"</b>'s nickname are now changed to &nbsp;<b> "${color(n.displayName, n.displayHexColor)}"</b>&nbsp;in [${color(n.guild.name, "lightgreen")}]`;
+			texting(format);
+		});
+		client.on("error", err =>{
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+			var format = `(${date}) ${color("[ERROR!] ", "orange")} An error occured with output >> ${color(err.message, "#aaaaaa")}`;
+			texting(format);
+
+		});
+		client.on("disconnect", err =>{
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+			var format = `(${date}) ${color("[DISCONNECT!] ", "orange")} Disconnected from Server. >> ${color(err.code, "#aaaaaa")} - ${color(err.reason, "#aaaaaa")}`;
+			texting(format);
+
+		});
+		function del(c, id){
+			if(confirm("Are you sure to delete this message?")){
+			client.channels.get(c).fetchMessage(id).then(msg=>{msg.react('🗑️');msg.delete(1000)}).catch(console.log("ERROR!"));}
+		};
+		function dm(a){
+			var b = prompt("Put a message to send it to a user");
+			if(!b) return alert("your message wasnt send because its blank");
+			client.users.get(a).send(b);
+		}
+		function gm(b){
+			var c = prompt("Put a message to send it to channel");
+			if(!c) return alert("your message wasnt send because its blank");
+				client.channels.get(b).send(c);
+		}
+		client.login(token)
---
+++ js/vanilla-main.js
@@ -0,0 +1,19 @@
+
+		texting("Wait Till the client ready")
+		var a = 0;
+
+		function texting(input, content, msg){
+			a++;
+			document.getElementById('a').outerHTML += `<span class="msg-line"><span class="hover-line"></span>${input}</span><br/>`;
+		}
+        function color(s, c) {
+            var res = '<font style="color:'+c+'">' + s + '</font>';
+            return res;
+        }
+		function Test(g, o){
+			document.getElementById(o).value = g;
+		}
+		if(!token){
+			const today = new Date(), date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+			var format = `(${date}) <font color="orange">[ERROR]</font> An error occured with output >> <font color="#aaa">PUT THE BOT TOKEN!!! Dont worry this HTML wont steal your bot TOKEN</font>`;
+			texting(format);};
